ee009834d448daaf65bf22b2a8699d0b6d062d81
whauzan <wahyu172845@gmail.com>
2023-08-14 20:06:53 +0700

    feat: auth page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,16 +3,36 @@ import ReactDOM from 'react-dom/client';
 import App from './pages/App.tsx';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './pages/Login.tsx';
+import { AuthProvider } from './context/AuthContext.tsx';
+import AuthLayout from './components/layout/AuthLayout.tsx';
+import Register from './pages/Register.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      }
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
